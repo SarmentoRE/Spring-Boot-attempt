26a2509ef909423ca38aacdf39369d697484e0ca
Austin Sarmento <austins0692@gmail.com>
2017-12-07 23:51:13 -0500

    with controller

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,5 +1,49 @@
 package controller;
 
-public class MainController {
+import java.security.Principal;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	
+	
+	@RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
+	public String logoutSuccessfulPage(Model model) {
+	    model.addAttribute("title", "Logout");
+	    return "logoutSuccessfulPage";
+	}
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage(Model model) {
+        return "loginPage";
+    }
+	
+	@RequestMapping(value = "/userInfo", method = RequestMethod.GET)
+	public String userInfo(Model model, Principal principal) {
+		String userName = principal.getName();
+		System.out.println("User Name: " + userName);
+		return "userInfoPage";
+	}
+	
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+    public String accessDenied(Model model, Principal principal) {
+        if (principal != null) {
+            User loginedUser = (User) ((Authentication) principal).getPrincipal();
+            String userInfo = loginedUser.getUsername();
+            model.addAttribute("userInfo", userInfo);
+ 
+            String message = "Hi " + principal.getName() //
+                    + "<br> You do not have permission to access this page!";
+            model.addAttribute("message", message);
+ 
+        }
+ 
+        return "403Page";
+    }
 }
